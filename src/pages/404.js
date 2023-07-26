f2c03e40ecc2f1a6b2414297808d59b5a126fb8b
Fahad <205fahadmohammad@gmail.com>
2023-07-26 22:44:12 +0600

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>404!!! Oops Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
